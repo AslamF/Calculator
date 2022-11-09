6600bd84dba7f9e356ca2c08171f4a1b4b1dbdc5
Fraz Aslam <faslam7646@gmail.com>
2022-11-08 19:59:27 -0500

    minor changes added operator logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let add = function(a, b){
-    let total = a + b;
+    let total = parseInt(a) + parseInt(b);
     return total;
 };
 console.log(add(500, 555));
@@ -32,7 +32,7 @@ let operate = function(operator, a, b){
         }
         else if (operator === "*"){
             return multiply(a, b);
-        }
+        } 
         else if (operator === "/"){
             return divide(a, b);
         }
@@ -40,28 +40,104 @@ let operate = function(operator, a, b){
             return "need an operator!"
         }
 };
-console.log(operate("", 25, 5));
+console.log(operate("-", 25, 5));
 
 
-const buttons = document.querySelectorAll("button");
+const divideButton = document.getElementById("#divide");
+const multiplyButton = document.getElementById("#multiply");
+const subtractButton = document.getElementById("#subtract");
+const additionButton = document.getElementById("#addition");
+const equalButton = document.getElementById("#equal");
+const clearButton = document.querySelector("#clear");
+
+const operators = document.querySelectorAll(".operator");
+
+
+const buttons = document.querySelectorAll(".number");
+const buttonsArray = Array.prototype.slice.call(buttons);
 const content = document.querySelector(".screen");
 
 var displayValue = "";
+var displayValue2 = "";
+var initialValue;
+var secondNumber;
+var total;
 
 
-
-buttons.forEach(button=>{
-    button.addEventListener("click", calculate);
+let initialNumber = buttons.forEach(button =>{
+    button.addEventListener("click", createNumber);
+    
+    
 })
 
 
-function calculate(){
-      let buttonValue = this.innerText;
-      displayValue += buttonValue
-      content.innerHTML ="";
-      content.append(displayValue);
-      console.log(displayValue)
+function createNumber(){
+    buttonValue = this.innerText;
+    displayValue += buttonValue;
+    content.innerHTML="";
+    content.append(displayValue);
+    parseInt(displayValue);
+    
 }
+
+
+
+
+operators.forEach(operator =>{
+    operator.addEventListener("click", saveOperation);
+    
+});
+
+function saveOperation(){
+    console.log(displayValue);
+    initialValue = displayValue;
+    displayValue = "";
+    operatorValue = this.innerText;
+    content.append(operatorValue);
+    if(operatorValue === "+"){
+        console.log(add(initialValue, 3))
+
+    }
+
+    
+}
+
+
+
+
+
+
+/*
+buttons.forEach(button=>{
+    button.addEventListener("click", calculate);
+   
+})
+
+function checkValue(){
+    console.log(displayValue)
+};
+
+function calculate(){
+    
+      let buttonValue = parseInt(this.innerText);
+      displayValue = displayValue + buttonValue;
+      content.innerHTML = "";
+      content.append(displayValue);  
+      checkValue();   
+    
+      
+};
+
+*/
+
+
+
+
+
+
+
+
+
 
 
 
