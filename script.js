9e3fb9bc55e775889f1ebdf611a9dc19ad3ad168
Fraz Aslam <faslam7646@gmail.com>
2022-11-12 03:28:30 -0500

    finished with the logic, need to fix visuals and it should be good. refactor code as well.

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let add = function(a, b){
     let total = parseInt(a) + parseInt(b);
     return total;
 };
-console.log(add(500, 555));
+
 
 let subtract = function(a, b){
     let total = a - b;
     return total;
 };
-console.log(subtract(50, 25));
+
 
 let multiply = function(a, b){
     let total = a * b;
     return total;
 };
-console.log(multiply(50, 50));
+
 
 let divide = function(a, b){
     let total = a / b;
     return total;
 };
-console.log(divide(25, 5));
+
 
 let operate = function(operator, a, b){
     
@@ -40,9 +40,180 @@ let operate = function(operator, a, b){
             return "need an operator!"
         }
 };
-console.log(operate("-", 25, 5));
 
 
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector("#clear");
+const decimal = document.querySelector(".decimal");
+const backspace = document.querySelector("#backspace");
+
+
+const screen = document.querySelector(".screen");
+
+
+let operator ="";
+let initialValue = "";
+let currentValue="";
+let total = "";
+
+
+numbers.forEach((number) => number.addEventListener("click", (button)=>{
+    handleNumber(button.target.textContent);
+    
+}))
+
+operators.forEach((operator) =>  operator.addEventListener("click", (button)=>{
+    handleOperator(button.target.textContent);
+    
+    
+    
+}));
+
+equal.addEventListener("click", function(){
+    if (currentValue !="" && initialValue !=""){
+    calculate();
+    currentValue = initialValue;
+    };
+})
+
+clear.addEventListener("click", clearAll);
+
+decimal.addEventListener("click", addDecimal);
+
+backspace.addEventListener("click", back);
+
+window.addEventListener('keydown', inputfunction) ;
+
+
+function clearAll(){
+    screen.textContent = "";
+    initialValue ="";
+    currentValue ="";
+    operator ="";
+    if(currentValue === ""){
+        screen.textContent = "0";
+    }
+}
+
+function handleNumber(num){
+
+   
+    if(currentValue.length <= 7){
+        currentValue += num;
+    };
+    screen.textContent = currentValue;
+}
+
+function handleOperator(op){
+ 
+    operator = op;
+    initialValue = currentValue;
+    currentValue = "";
+    
+}
+
+
+
+function calculate(){
+    initialValue = Number(initialValue);
+    currentValue = Number(currentValue);
+    
+
+    if (operator === "+"){
+        initialValue += currentValue;
+        console.log(initialValue);
+    }
+
+    else if (operator === "-"){
+        initialValue -= currentValue;
+        console.log(initialValue);
+    }
+    else if (operator === "X"){
+        initialValue *= currentValue;
+        console.log(initialValue);
+    }
+   
+    else if(operator === "/"){
+        if(currentValue === 0){
+            initialValue = "ERROR";
+            screen.textContent ="";
+            screen.textContent = initialValue;
+            operator ="";
+            return;
+        }
+        initialValue /= currentValue;
+        console.log(initialValue);
+    }
+    
+    
+    initialValue = Math.round(initialValue * 1000) / 1000;
+
+    initialValue = initialValue.toString();
+    currentValue = currentValue.toString();
+    screen.textContent = initialValue;
+
+}
+
+function addDecimal(){
+    console.log("cicked")
+    if(!currentValue.includes(".")){
+        currentValue += ".";
+    }
+    return;
+
+}
+
+function back(){
+    if (currentValue != ""){
+        currentValue = currentValue.slice(0, -1);
+    }
+   
+    screen.textContent = currentValue;
+    if(currentValue === ""){
+        screen.textContent ="0"
+    }
+    
+
+}
+
+
+
+
+function inputfunction(e){
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9){
+        handleNumber(e.key);
+    }
+    if (e.key ==="Enter" || (e.key === "=") && currentValue!= "" && initialValue != ""){
+        if (currentValue !="" && initialValue !=""){
+            calculate();
+            currentValue = initialValue;
+            };
+        
+    }
+
+    if (e.key === "+" || e.key == "-" || e.key === "/"){
+        handleOperator(e.key);
+    }
+    if (e.key === "*"){
+        handleOperator("X");
+    }
+    if (e.key === "."){
+        addDecimal();
+    }
+    if (e.keyCode === 8){
+        back();
+        console.log("clicked")
+    }
+}
+
+
+
+
+
+/*
 const divideButton = document.getElementById("#divide");
 const multiplyButton = document.getElementById("#multiply");
 const subtractButton = document.getElementById("#subtract");
@@ -58,15 +229,13 @@ const buttonsArray = Array.prototype.slice.call(buttons);
 const content = document.querySelector(".screen");
 
 var displayValue = "";
-var displayValue2 = "";
-var initialValue;
-var secondNumber;
-var total;
+var secondValue = "";
+var operatorValue;
 
 
-let initialNumber = buttons.forEach(button =>{
+buttons.forEach(button =>{
     button.addEventListener("click", createNumber);
-    
+    button.addEventListener("click", secondNumber)
     
 })
 
@@ -74,12 +243,13 @@ let initialNumber = buttons.forEach(button =>{
 function createNumber(){
     buttonValue = this.innerText;
     displayValue += buttonValue;
-    content.innerHTML="";
-    content.append(displayValue);
+    content.textContent = displayValue;
     parseInt(displayValue);
-    
+
 }
 
+function secondNumber(){
+    
 
 
 
@@ -91,16 +261,14 @@ operators.forEach(operator =>{
 function saveOperation(){
     console.log(displayValue);
     initialValue = displayValue;
+    content.textContent = "";
     displayValue = "";
+    displayValue = secondValue;
     operatorValue = this.innerText;
-    content.append(operatorValue);
-    if(operatorValue === "+"){
-        console.log(add(initialValue, 3))
-
-    }
-
     
+   
 }
+
 
 
 
